1cb9c60531344f1be42ff4804a6a3be7a1394841
Cao Van Cuong <dev.wdsoft@gmail.com>
2025-04-13 16:30:46 +0700

    update code

--- src/main/kotlin/teams/TeamImageCrawler.kt
+++ src/main/kotlin/teams/TeamImageCrawler.kt
@@ -19,9 +19,9 @@ object TeamImageCrawler {
 
     fun fetchTeamImages() {
         initData()
-        val teams = TeamExtractor.fetchTeams(15)
-            .plus(TeamExtractor.fetchTeams(16))
-            .plus(TeamExtractor.fetchTeams(17))
+        val teams = TeamExtractor.fetchTeams(18)
+            .plus(TeamExtractor.fetchTeams(19))
+            .plus(TeamExtractor.fetchTeams(20))
             .filter { !it.Img.isNullOrBlank() }
             .distinctBy { it.ID }
         println("\uD83D\uDE80 \uD83D\uDE80 \uD83D\uDE80FETCHED ${teams.size} TEAMS FROM API")
