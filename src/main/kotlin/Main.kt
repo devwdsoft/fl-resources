7210da8071e5d1dd99dca775e744ae1836a27f80
Cao Van Cuong <dev.wdsoft@gmail.com>
2025-04-13 10:19:56 +0700

    update code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,9 @@
-import teams.TeamImageCrawler
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 //    TeamImageCrawler.fetchTeamImages()
     println("================================>")
     println(System.getenv())
-    println(System.getenv("BASE_SCHEDULE_API"))
+    println("VCL: " + System.getenv("BASE_SCHEDULE_API"))
     println("================================>")
 }
