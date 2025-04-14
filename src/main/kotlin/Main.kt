4a180aef9d2415f25763426a2ca0f8a156b873a4
CuongCV <cuongcv@CuongCVs-MacBook-Pro.local>
2025-04-14 10:02:31 +0700

    create team cron flow

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,7 @@
-import java.io.File
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Date
+import teams.TeamImageCrawler
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    TeamImageCrawler.fetchTeamImages()
 }
