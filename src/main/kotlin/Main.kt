c81766fc772973c33b49db0feab20381477a42fb
CuongCV <cuongcv@CuongCVs-MacBook-Pro.local>
2025-04-14 10:01:15 +0700

    update workflows

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,13 +10,4 @@ fun main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!")
-    val file = File("assets/test.txt")
-    file.appendText("\n${SimpleDateFormat().format(Date())}")
-    println("VCL: ${file.exists()} ${file.absolutePath}")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
 }
