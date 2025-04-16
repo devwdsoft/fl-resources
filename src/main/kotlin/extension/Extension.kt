c90a7df57819bd3505d382ccdde6579f2b8c25c4
CuongCV <cuongcv@CuongCVs-MacBook-Pro.local>
2025-04-16 16:50:43 +0700

    add getEnv extension

--- src/main/kotlin/extension/Extension.kt
+++ src/main/kotlin/extension/Extension.kt
@@ -2,3 +2,12 @@ package extension
 
 import io.github.cdimascio.dotenv.dotenv
 
+fun getEnv(key: String): String? {
+    val isGithubCI = System.getenv("GITHUB_ACTIONS") == "true"
+
+    return if (isGithubCI) {
+        System.getenv(key) // dùng GitHub Secrets
+    } else {
+        dotenv()[key]      // dùng .env local
+    }
+}
