27b1c3841a3a1f21fb2d2c67ba07070a64c3d5c3
CuongCV <cuongcv@CuongCVs-MacBook-Pro.local>
2025-04-16 16:50:43 +0700

    add getEnv extension

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.9.2")
 
     testImplementation(kotlin("test"))
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
 }
 
 application {
